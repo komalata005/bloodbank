304956ff6c5d18fcc64bee35ede4351354e6dadb
komalata005 <komal.ata005@gmail.com>
2024-03-05 22:21:58 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './styles/main.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Features from './components/Features';
-import Home from './pages/home';
 
 function App() {
   return (
-    <><Header /><Footer /></>
+    <>
+    <h1 className="text-3xl font-bold text-red-700 underline">
+      Hello world!
+    </h1>
+    <Header /><Footer />
+    </>
   );
 }
 
